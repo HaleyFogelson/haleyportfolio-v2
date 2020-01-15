897dacc8e349028e2bf065c463e2b71b945aaf82
Haley Fogelson <fogelson.h@husky.neu.edu>
2020-01-15 13:11:00 -0500

    Updates

---
+++ precache-manifest.32b6457fe9e07c0ff23eacacfa30c7f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9484339cecd5e932cff306cda2da22",
+    "url": "/haleyportfolio-v2/index.html"
+  },
+  {
+    "revision": "6d79cce795ce6e06ca7c",
+    "url": "/haleyportfolio-v2/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d5195832a96daa3fd639",
+    "url": "/haleyportfolio-v2/static/css/main.ec60dad2.chunk.css"
+  },
+  {
+    "revision": "6d79cce795ce6e06ca7c",
+    "url": "/haleyportfolio-v2/static/js/2.d5a46b4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/haleyportfolio-v2/static/js/2.d5a46b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5195832a96daa3fd639",
+    "url": "/haleyportfolio-v2/static/js/main.5891cf8d.chunk.js"
+  },
+  {
+    "revision": "d5ec7d5a02dbb8afbfa3",
+    "url": "/haleyportfolio-v2/static/js/runtime-main.2a762b64.js"
+  },
+  {
+    "revision": "44ae60facdf5598b04e6bea671404d4c",
+    "url": "/haleyportfolio-v2/static/media/SmartyPill.44ae60fa.jpg"
+  },
+  {
+    "revision": "44ae60facdf5598b04e6bea671404d4c",
+    "url": "/haleyportfolio-v2/static/media/Vmarine.44ae60fa.jpg"
+  },
+  {
+    "revision": "0e647d39f4a4acf9a1c18550627b5a1c",
+    "url": "/haleyportfolio-v2/static/media/agueDeLuz.0e647d39.png"
+  },
+  {
+    "revision": "594d1b2bb24bd440c6c961ad65353882",
+    "url": "/haleyportfolio-v2/static/media/myImage.594d1b2b.png"
+  },
+  {
+    "revision": "3c5c1972d3affc847e6bccf61b406410",
+    "url": "/haleyportfolio-v2/static/media/todolist.3c5c1972.png"
+  }
+]);
