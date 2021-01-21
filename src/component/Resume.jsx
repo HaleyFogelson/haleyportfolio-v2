04cfcd2ce5a80e07a5ee41b79f93cae8d3b967bb
Haley Fogelson <33294092+HaleyFogelson@users.noreply.github.com>
2021-01-21 11:09:38 -0500

    Update Resume

--- src/component/Resume.jsx
+++ src/component/Resume.jsx
@@ -12,16 +12,16 @@ export default class Resume extends React.Component {
 		<h3 style={{ color: 'blue' }}> Education </h3>
 		
 		<h6> <h5> Northeastern Univerisity</h5> Boston, MA <br/> 
-		Bachelor of Science in Computer Engineering and Computer Science, GPA: 3.87  <br/>
+		Bachelor of Science in Computer Engineering and Computer Science with Minor in Entrepreneurial Startups, GPA: 3.88  <br/>
 		Dean’s List: Fall 2018-present  <br/> <br/>
 		<div style={{ display:'flex'}}>
-		<h5>Courses:</h5>      Fund. of Computer Science 2, Calculus 2, Chemistry 2,  Physics 2, Differential Equations and Linear Algebra, Embedded 
-	Design, Discrete Structures, Circuits and Signals, Fund. of Networks, Algorithms, Logic and Computation
+		<h5>Courses:</h5>      Artificial Intelligence, Object Oriented Design, Computer Systems, Digital Design, Fund. of Computer Science 2, Calculus 2, Chemistry 2,  Physics 2,
+		 Differential Equations and Linear Algebra, Embedded Design, Discrete Structures, Circuits and Signals, Fund. of Networks, Algorithms, Logic and Computation
 	</div>
 	<br/> <br/>
 	<div style={{ display:'flex'}}>
-	<h5>Activities:</h5>   Generate (Build Studio Engineer), Hillel (Learning Chair & Community Engagement Intern), Delta Phi Epsilon, SWE,    
-                    IEEE, NUHOC, and Club running
+	<h5>Activities:</h5>   Generate (Build Studio Engineer & Website Lead), Hillel (Learning Chair & Community Engagement Intern), Delta Phi Epsilon(Panhel Delegate), SWE,    
+                    IEEE, NUHOC, CHAARG and Club running
      </div>
 		<br/><br/>
 		<h3 style={{ color: 'blue' }}> Projects </h3>
@@ -38,6 +38,14 @@ export default class Resume extends React.Component {
 					</button>
 		<br/><br/> 
 		<h3 style={{ color: 'blue' }}> Work Experience </h3>
+		<h5> CLYPD and Xandr </h5> Boston, MA <br/> July-December 2020 <br/> Software Engineering CO-OP <br/>
+		<li>Worked on a development team for Clypd which was aquired by Xandr while I worked there </li>
+		<li>Fully integrated into a team of engineers and made signifigent contributions to the code base</li> 
+		<li>Led an Epic to create useful tools for the company</li> 
+		<li>Did sprint planning in Jira</li>
+		<li>Learned the language Go</li>
+		<li>Learned about how to make teams efficient and how a company runs well by meeting with executives</li>
+		<br/><br/>
 		<h5> AT&T Cyber Security </h5> Columbia, MD <br/> May-August 2019 <br/> Goverment solutions Intern <br/>
 		<li>Interned in the cyber security government branch for AT&T </li>
 		<li>Researched new technologies and created presentations to teach full-time employees new skills</li> 
