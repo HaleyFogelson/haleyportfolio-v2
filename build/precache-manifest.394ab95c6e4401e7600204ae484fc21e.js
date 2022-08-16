a3a0413b59afc1cfbb093c98ea4e4556f8963495
Haley Fogelson <fogelsonhaley@gmail.com>
2022-08-15 20:25:31 -0400

    updates

---
+++ build/precache-manifest.394ab95c6e4401e7600204ae484fc21e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080336217036c4e9a781a039cda1e8ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae5ea7e98df88d60eabc",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "d181e3664ee0c0f94e40",
+    "url": "/static/css/main.1dcb7d03.chunk.css"
+  },
+  {
+    "revision": "ae5ea7e98df88d60eabc",
+    "url": "/static/js/2.0ba6f21f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.0ba6f21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d181e3664ee0c0f94e40",
+    "url": "/static/js/main.444b8b37.chunk.js"
+  },
+  {
+    "revision": "b0a724d511f8a8681bc4",
+    "url": "/static/js/runtime-main.db9ed87d.js"
+  },
+  {
+    "revision": "f515acc33349ed9a2b926dbd60eb5f01",
+    "url": "/static/media/GoImageRecoginition.f515acc3.png"
+  },
+  {
+    "revision": "86ee0b9164be197ce9a116aa7ee4c44e",
+    "url": "/static/media/IMG_0441.86ee0b91.jpg"
+  },
+  {
+    "revision": "f5d8911389b59022fc489bc0d57511e9",
+    "url": "/static/media/Maskup.f5d89113.png"
+  },
+  {
+    "revision": "44ae60facdf5598b04e6bea671404d4c",
+    "url": "/static/media/SmartyPill.44ae60fa.jpg"
+  },
+  {
+    "revision": "44ae60facdf5598b04e6bea671404d4c",
+    "url": "/static/media/Vmarine.44ae60fa.jpg"
+  },
+  {
+    "revision": "0e647d39f4a4acf9a1c18550627b5a1c",
+    "url": "/static/media/agueDeLuz.0e647d39.png"
+  },
+  {
+    "revision": "ac312a3f3fbfaa82c1869137983082a8",
+    "url": "/static/media/groworganic.ac312a3f.png"
+  },
+  {
+    "revision": "365d9379d5314452e8a8fde29390558c",
+    "url": "/static/media/logo.365d9379.png"
+  },
+  {
+    "revision": "50831a2de0694c47d3d4d0b2db47e676",
+    "url": "/static/media/myImage.50831a2d.png"
+  },
+  {
+    "revision": "52f9c47d3eb9a339804d0236c82962cf",
+    "url": "/static/media/roboArdunio.52f9c47d.png"
+  },
+  {
+    "revision": "e18973ae9fb809038ebe780713a82e80",
+    "url": "/static/media/roboarm.e18973ae.mov"
+  },
+  {
+    "revision": "2c076d84b2de0831a2efe196282624c3",
+    "url": "/static/media/roboarmpic.2c076d84.png"
+  },
+  {
+    "revision": "fcf75ab6f23abfb86e36507a05e846ee",
+    "url": "/static/media/smarttrash.fcf75ab6.jpg"
+  },
+  {
+    "revision": "3abab01c3edcd1d136267fed04de156d",
+    "url": "/static/media/todolist.3abab01c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d5642b5c2683db7ebed740e707533ab.js"
+  "/precache-manifest.394ab95c6e4401e7600204ae484fc21e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
